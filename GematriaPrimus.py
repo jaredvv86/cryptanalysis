f08c4eaf6f99aa5dd2eead853f10833dd787b7a6
Jared van der Veen <jaredvv86@gmail.com>
2020-09-07 21:53:16 -0500

    Add files via upload

--- GematriaPrimus.py
+++ GematriaPrimus.py
@@ -4,140 +4,37 @@
 in the GP
 """
 
-integers=[x for x in range(0,29)]
-runes=("ᚠ","ᚢ","ᚦ","ᚩ","ᚱ","ᚳ","ᚷ","ᚹ","ᚻ","ᚾ","ᛁ","ᛄ","ᛇ","ᛈ","ᛉ","ᛋ","ᛏ","ᛒ","ᛖ","ᛗ",
+## For start index of 0 choose start = 0 and stop = 29
+## For start index of 1 choose start = 1 and stop = 30
+start=0
+stop=29
+INTEGERS=[x for x in range(start,stop)]
+
+RUNES=("ᚠ","ᚢ","ᚦ","ᚩ","ᚱ","ᚳ","ᚷ","ᚹ","ᚻ","ᚾ","ᛁ","ᛄ","ᛇ","ᛈ","ᛉ","ᛋ","ᛏ","ᛒ","ᛖ","ᛗ",
        "ᛚ","ᛝ","ᛟ","ᛞ","ᚪ","ᚫ","ᚣ","ᛡ","ᛠ")
-primes=(2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,
+
+PRIMES=(2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,
         103,107,109)
-latin=["F", "U","TH","O","R","K","G","W","H","N","I","J","EO","P","X","Z","T","B","E",
+
+LATIN=["F", "U","TH","O","R","K","G","W","H","N","I","J","EO","P","X","Z","T","B","E",
        "M","L","ING","OE","D","A","AE","Y","IA","EA"]
+
 def prime_to_latin(prime):
-    mapping = dict(zip(primes, latin))
+    mapping = dict(zip(PRIMES, LATIN))
     return mapping[prime]
 
 def latin_to_prime(latin):
-    mapping = {
-		"F": 2,
-        "U": 3,
-        "TH":5,
-        "O": 7,
-        "R":11,
-        "K":13,
-        "G":17,
-        "W":19,
-        "H":23,
-        "N":29,
-        "I":31,
-        "J":37,
-        "EO":41,
-        "P":43,
-        "X":47,
-        "Z":53,
-        "T":59,
-        "B":61,
-        "E":67,
-        "M":71,
-        "L":73,
-        "ING":79,
-        "OE":83,
-        "D":89,
-        "A":97,
-        "AE":101,
-        "Y":103,
-        "IA":107,
-        "EA":109}
-    try:
-        return mapping[latin]
-    except KeyError as e:
-            print(e," IS NOT IN THE MAPPING" )
-            raise e
+    mapping = dict(zip(LATIN, PRIMES))
+    return mapping[latin]
 
 def prime_to_rune(prime):
-    '''
-    defines the mapping from primes runic charaters according to the GP.
-    Raises an exception if the input is NOT prime
-    '''
-    mapping = {
-        2:"ᚠ",
-        3:"ᚢ",
-        5:"ᚦ",
-        7:"ᚩ",
-        11:"ᚱ",
-        13:"ᚳ",
-        17:"ᚷ",
-        19:"ᚹ",
-        23:"ᚻ",
-        29:"ᚾ",
-        31:"ᛁ",
-        37:"ᛄ",
-        41:"ᛇ",
-        43:"ᛈ",
-        47:"ᛉ",
-        53:"ᛋ",
-        59:"ᛏ",
-        61:"ᛒ",
-        67:"ᛖ",
-        71:"ᛗ",
-        73:"ᛚ",
-        79:"ᛝ",
-        83:"ᛟ",
-        89:"ᛞ",
-        97:"ᚪ",
-        101:"ᚫ",
-        103:"ᚣ",
-        107:"ᛡ",
-        109:"ᛠ"}
-    try:
-        return mapping[prime]
-    except KeyError as e:
-        print(e," IS NOT IN THE MAPPING" )
-        raise e	
+	mapping = dict(zip(PRIMES, RUNES))
+	return mapping[prime]
 
 def rune_to_prime(rune):
-    mapping = dict(zip(runes, primes))
+    mapping = dict(zip(RUNES, PRIMES))
     return mapping[rune]
 
-def rune_to_seq(rune,zero=False):
-    '''
-    defines the mapping from runic to sequential according to the GP.
-    Raises an exception if the input is NOT prime
-    '''
-    if zero==False:
-        z=0
-    else:
-        z=1
-    mapping = {
-        "ᚠ":1-z,
-        "ᚢ":2-z,
-        "ᚦ":3-z,
-        "ᚩ":4-z,
-        "ᚱ":5-z,
-        "ᚳ":6-z,
-        "ᚷ":7-z,
-        "ᚹ":8-z,
-        "ᚻ":9-z,
-        "ᚾ":10-z,
-        "ᛁ":11-z,
-        "ᛄ":12-z,
-        "ᛇ":13-z,
-        "ᛈ":14-z,
-        "ᛉ":15-z,
-        "ᛋ":16-z,
-        "ᛏ":17-z,
-        "ᛒ":18-z,
-        "ᛖ":19-z,
-        "ᛗ":20-z,
-        "ᛚ":21-z,
-        "ᛝ":22-z,
-        "ᛟ":23-z,
-        "ᛞ":24-z,
-        "ᚪ":25-z,
-        "ᚫ":26-z,
-        "ᚣ":27-z,
-        "ᛡ":28-z,
-        "ᛠ":29-z}
-    try:
-        return mapping[rune]
-    except KeyError as e:
-        print(e," IS NOT IN THE MAPPING" )
-        raise e 
+def rune_to_seq(rune):
+    mapping = dict(zip(RUNES, INTEGERS))
+    return mapping[rune]
--- LiberPrimus.py
+++ LiberPrimus.py
@@ -6,14 +6,27 @@ import GematriaPrimus as GP
 
 page40="""ᚠᚾᛗᚣᚷᛞᚫᚻᚪᛈᛉᚣᚻᛇᛠᚩᛖᛏᛝᛠᛚᛁᛏᚦᚠᛗᚪᚳᛖᛞᚳᛏᚱᛟᚷᛠᚾᚫᛒᚢᛖᛒᚢᚦᚠᛟᚷᛋᛟᛁᛈᛟᛉᛋᛒᚹᛄᛒᚣᛗᚢᛠᚱᛁᚢᛟᛄᛁᛗᛖᚫᚱᛋᛉᛝᛠᛈᛚᛞᚩᛚᛁᛉᛠᛝᛖᚱᚾᛈᛖᚹᛡᚾᛄᛏᚣᛋᚩᛋᛏᛝᚢᚾᛇᚪᛖᛏᚪᛄᚳᚣᛟᛒᛚᛋᛒᛞᛄᛁᛝᚣᛖᚳᛄᚻᛚᚣᚷᚫᛚᛞᛚᚫᛚᚦᛉᛚᛖᛉᚩᛉᛁᚳᚢᛗᚾᚢᚩᚾᛇᚻᛡᛚᛇᚩᚫᚪᚩᛟᚩᚣᚱᛖᚠᚢᛁᚻᛟᛚᚾᛏᚠᛞᚱᛠᚷᛈᚩᛇᚩᛗᛠᛒᛄᛡᛋᛗᚠᛏᚠᚫᚩᛟᚳᛚᛞᛡᛚᚩᚳᛝᚢᛈᚹᛏᚷᚳᛋᚢᛟᚷᚦᚠᛉᚠᛏᚳᛋᛉᛟᚷᚠᛉᚾᛞᛒᛏᛠᛡᛈᛡ"""
 
-class Page:
+class Page(object):
 	"""defines a Page object from the LP requires the rune text and the LP page number"""
-	def __init__(self, rune_text, page_number):
+	def __init__(self, rune_text, page_number=None):
 		self.rune_text = rune_text
 		self.page_number = page_number
+		self.unique_chars = [char for char in set(self.rune_text)]
 
-	def rune_to_prime(self,):
-		return [GP.rune_to_prime(rune) for rune in self.rune_text]
+	def get_prime_form(self):
+		
+		#return [GP.rune_to_prime(rune) for rune in self.rune_text]
+
+	def get_seq_form(self):
+		return [GP.rune_to_seq(rune) for rune in self.rune_text]
+
+	def get_unique_chars(self):
+		return self.unique_chars
+
+	def get_char_counts(self):
+		pass
 
 p40=Page(page40, 40)
-print(p40.rune_to_prime())
+#print(p40.get_prime_form())
+#print(p40.get_seq_form())
+print(p40.get_unique_chars().get_prime_form())
